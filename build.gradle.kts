f823b4e1e4b16282bfee9e8523ce9bb5ce0e0e15
Hiddify <114227601+hiddify1@users.noreply.github.com>
2023-04-25 09:51:25 +0000

    22

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ subprojects {
             minSdk = 21
             targetSdk = 31
 
-            versionName = "2.7.3-h0.21"
-            versionCode = 207003021
+            versionName = "2.7.3-h0.22"
+            versionCode = 207003022
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
